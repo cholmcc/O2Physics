a320e32f1fd6f79d6fcf1044a843644f4a6a6c84
Christian Holm Christensen <cholmcc@gmail.com>
2023-11-15 22:45:01 +0100

    Large commit for Rivet integration
    This commit imports the [O2Rivet](https://gitlab.com/cholmcc/O2Rivet)
    project into O2Physics.
    
    This project aims to enable running Rivet analyses on existing
    and future simulation productions, locally, on CVMFS, on Grid,
    and with HyperLoop.
    
    The main component is the DPL `o2-analysis-mm-rivet`.  This reads in MC
    AODs (via f.ex. `o2-aod-mc-producer-workflow`), converts the events to
    HepMC events, and then runs Rivet analyses on these.  The output is an
    `o2::rivet::RivetAOs` object stored in the generated
    `AnalysisResults.root` file.
    
    The tool supports partial runs (e.g., parallel processing on Grid), and
    merging, including the `Rivet::Analysis::finalize` step, via regular
    ROOT merging.
    
    The tool also supports compiling analyses on the fly.  This is useful in
    Grid or similar settings, and for developing and testing analyses not
    yet accepted into an official repository.
    
    Examples of using the tool are given in `PWGMM/Rivet/doc` along with
    some other documentation.
    
    Compilation of the tool is contingent on the presence of Rivet (and its
    requirements YODA, FastJetContrib, and HepMC3) on the target system.
    The feature is marked as _recommended_ via optional, but recommended,
    build dependencies on Rivet and YODA.

---
+++ PWGMM/Rivet/doc/analyses/ALICE_YYYY_I1234567.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env -S python -i --
+
+def plotit(infile,raw):
+    from matplotlib.pyplot import gca
+    from yoda import readYODA
+    from sys import stderr
+
+    aos = readYODA(infile)
+
+    if aos is None:
+        raise RuntimeError(f'No AOS read from {infile}')
+
+    histo = None
+    nev   = None
+    xsec  = None
+
+    for prefix in ["", "/RAW"]:
+        histo = aos.get(prefix+'/ALICE_YYYY_I1234567/d01-x01-y01',None)
+        nev   = aos.get(prefix+'/_EVTCOUNT',None)
+        xsec  = aos.get(prefix+'/_XSEC',None)
+        if histo is not None and \
+           histo.effNumEntries() > .1 and \
+           nev is not None and \
+           xsec is not None:
+            break
+
+    if histo is None:
+        raise RuntimeError(f'Histogram not found in {infile}')
+
+    ax = gca()
+    ax.errorbar(histo.xMids(),
+                histo.yVals(),
+                histo.yErrs(),
+                histo.xWidths(),
+                'o')
+    ax.set_xlabel(r'$\eta$')
+    ax.set_ylabel(r'$\mathrm{d}N_{\mathrm{ch}}/\mathrm{d}\eta$')
+    ax.set_title(f'{int(nev.val())} events '
+                 f'{"("+prefix+")" if len(prefix)>0 else ""}')
+
+    ax.figure.show()
+    ax.figure.tight_layout()
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser, FileType
+
+    ap = ArgumentParser(description='Plot results')
+    ap.add_argument('input',nargs='?',default='AO2D_LHC23d1f_520259_001.yoda',
+                    help='Input file',
+                    type=FileType('r'))
+    ap.add_argument('-r','--raw',default=False,
+                    help='Show raw results')
+
+    args = ap.parse_args()
+    plotit(args.input,args.raw)
+
+    print(f'Type Ctrl-D or write exit() to end')
+
+#
+# EOF
+#
---
+++ PWGMM/Rivet/doc/examples/README.md
@@ -0,0 +1,5 @@
+# Examples of Rivet runs over simulated data
+
+- [`pythia8pp.sh.in`](pythia8pp.sh.in) Example of running a simulation
+  with Pythia8 input, and then running an example Rivet analysis on
+  that data.
